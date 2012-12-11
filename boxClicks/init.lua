327a062a633771807b032cd15a492378e0444c7c
bluh <socks7899@gmail.com>
2012-12-10 20:23:10 -0500

    fixed some weird bug (maybe)

--- boxClicks/init.lua
+++ boxClicks/init.lua
@@ -77,7 +77,12 @@ boxClicks = {
 		for _,a in pairs(self.boxes) do
 			if a:check(x,y) then
 				--if kind ~= "move" then print(kind,x,y,b.id) end
-				if a.callback[kind] and self.paused[kind] == nil then a.callback[kind](a,kind,x,y) end
+				if a.callback[kind] and self.paused[kind] == nil then
+					s,e = pcall(a.callback[kind],a,kind,x,y)
+					if not s then
+						print(e)
+					end
+				end
 			end
 		end
 	end),
--- tronics/tronicslist.lua
+++ tronics/tronicslist.lua
@@ -84,7 +84,7 @@ TRANIX = {
 		ico = "/assets/dataIco.png",
 		sprite = "/assets/data.png",
 		nodes = {
-			{48,21,4} --let's just use 4 for data and never talk about it ever again
+			{48,21,4}, --let's just use 4 for data and never talk about it ever again
 		}
 		--data will be hardcoded and will not have a source
 	}
